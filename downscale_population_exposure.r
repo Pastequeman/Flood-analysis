ec0a7fe9bf9bba38d0fd31e96a6c6cb431c098c9
Julien Boulange <boulange.julien@gmail.com>
2020-06-23 15:28:54 +0900

    implementation of ssp1 to ssp5

--- downscale_population_exposure.r
+++ downscale_population_exposure.r
@@ -4,31 +4,46 @@ args = commandArgs(trailingOnly = TRUE)
 ## Purpose:
 # every year > compute ave. nb of people exposed to flood
 
+## history
+# 23/06 -> added support for ssp1 to ssp5, ncdf file covering 2006 to 2100
+
 ## libraries:
 library("tibble")
 library("readr")
 suppressMessages(library("dplyr"))
 library("ncdf4")
 ########### start inputs
-if (length(args) != 3 ) {
-  stop("error in cmd parameters > GCM=H2C/G2C_... | EXP=dam_trim/nodam_trim | country=yes/no")
+if (length(args) != 4 ) {
+  stop("error in cmd parameters > GCM=H2C/G2C_... | EXP=dam_trim/nodam_trim | country=yes/no, ssp=no/ssp1...ssp5")
 } else {
   GCMS     <- args[1] #"I2C_"
   EXP      <- args[2] #"nodam_trim"
   COUNTRY  <- args[3] # yes or no
+  SSP      <- args[4] # no, ssp1 ... ssp5
 }
 
 # other inputs
 step <- 0.005
 
-## clean
-if (file.exists(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/country_exposure.csv"))) {
-  file.remove(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/country_exposure.csv"))
+## clean and remove previous files
+if (SSP == "no") {
+  if (file.exists(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/country_exposure.csv"))) {
+    file.remove(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/country_exposure.csv"))
+  }
+  if (COUNTRY == "yes") {
+    if (file.exists(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/position_exposure.csv"))) {
+      file.remove(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/position_exposure.csv"))}
+  }
+} else {
+  if (file.exists(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/", SSP, "_country_exposure.csv"))) {
+    file.remove(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/", SSP, "_country_exposure.csv"))
+  }
+  if (COUNTRY == "yes") {
+    if (file.exists(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/", SSP, "_position_exposure.csv"))) {
+      file.remove(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/", SSP, "_position_exposure.csv"))}
+  }
 }
-if (COUNTRY == "yes") {
-  if (file.exists(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/position_exposure.csv"))) {
-    file.remove(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/position_exposure.csv"))}
-}
+
 
 ########### years
 if (GCMS == "H0C_" | GCMS == "M0C_" | GCMS == "G0C_" | GCMS == "I0C_") {
@@ -97,63 +112,79 @@ for (region in regions) {
     close(c_file) ; rm(c_file)
   }
   # check which of the initial 8 regions are concerned
-  initial_reg <- tibble(reg = seq(1, 8),
-                        lon = rep(c(-180, -90, 0, 90), times = 2),
-                        lat = rep(c(90, 0), each = 4),
-                        inc = 0)
-  if (region != "si2") {
-    for (i in 1:8) {
-      initial_reg$inc[i] <- ifelse((sum(seq(xstart, by = 0.005, length.out = xdef) %in% seq(initial_reg$lon[i], by = 0.005, length.out = 18000)) > 0) & (sum(seq(ystart, by = -0.005, length.out = ydef) %in% seq(initial_reg$lat[i], by = -0.005, length.out = 18000)) >0 ), 1, 0)    
-    }
-  } else {
-    for (i in 1:8) {
-      initial_reg$inc[i] <- ifelse((sum(c(seq(xstart, 180, by = 0.005), seq(-180, -165, by = 0.005)) %in% seq(initial_reg$lon[i], by = 0.005, length.out = 18000)) > 0) & (sum(seq(ystart, by = -0.005, length.out = ydef) %in% seq(initial_reg$lat[i], by = -0.005, length.out = 18000)) >0 ), 1, 0)    
-    }    
-  }
-  #print(initial_reg)
-  #} ## DEBUG
-  # get the population
-  for (i in initial_reg[initial_reg$inc == 1,]$reg) {
-    nc_data <- nc_open(paste0("/data01/julien/projects/camaflood/DAT/population/inter_", i,".nc"))
-    population <- ncvar_get(nc_data, "var1")
-    nc_close(nc_data)
-    #population <- scan(paste0("/data01/julien/projects/camaflood/DAT/population/interpolated_reg_", i, ".asc"), quiet = TRUE)
-    pop <- data.frame(lon = rep(seq((initial_reg[initial_reg$reg == i,]$lon + 0.0025), by = 0.005, length.out = 18000), times = 18000),
-                      lat = rep(seq((initial_reg[initial_reg$reg == i,]$lat - 0.0025), by = -0.005, length.out = 18000), each = 18000),
-                      dat = as.vector(population) )# no need to transpose here!
+
+
+
+  if (SSP == "no") {
+    # this will tell in which of the 8 regions the region is located 
+    initial_reg <- tibble(reg = seq(1, 8),
+                          lon = rep(c(-180, -90, 0, 90), times = 2),
+                          lat = rep(c(90, 0), each = 4),
+                          inc = 0)
     if (region != "si2") {
-      population_v2 <- pop %>% filter(lon > xstart & lon < (xstart+0.005*xdef), lat < ystart & lat > (ystart-0.005*ydef))      
+      for (i in 1:8) {
+        initial_reg$inc[i] <- ifelse((sum(seq(xstart, by = 0.005, length.out = xdef) %in% seq(initial_reg$lon[i], by = 0.005, length.out = 18000)) > 0) & (sum(seq(ystart, by = -0.005, length.out = ydef) %in% seq(initial_reg$lat[i], by = -0.005, length.out = 18000)) >0 ), 1, 0)    
+      }
     } else {
-      if (i == 1) {
-        population_v2 <- pop %>% filter(lon > -180 & lon < -165, lat < ystart & lat > (ystart-0.005*ydef))        
+      for (i in 1:8) {
+        initial_reg$inc[i] <- ifelse((sum(c(seq(xstart, 180, by = 0.005), seq(-180, -165, by = 0.005)) %in% seq(initial_reg$lon[i], by = 0.005, length.out = 18000)) > 0) & (sum(seq(ystart, by = -0.005, length.out = ydef) %in% seq(initial_reg$lat[i], by = -0.005, length.out = 18000)) >0 ), 1, 0)    
+      }    
+    }
+
+    # computation
+    ## population
+    for (i in initial_reg[initial_reg$inc == 1,]$reg) {
+      nc_data <- nc_open(paste0("/data01/julien/projects/camaflood/DAT/population/inter_", i,".nc"))
+      population <- ncvar_get(nc_data, "var1")
+      nc_close(nc_data)
+
+      pop <- data.frame(lon = rep(seq((initial_reg[initial_reg$reg == i,]$lon + 0.0025), by = 0.005, length.out = 18000), times = 18000),
+                        lat = rep(seq((initial_reg[initial_reg$reg == i,]$lat - 0.0025), by = -0.005, length.out = 18000), each = 18000),
+                        dat = as.vector(population) )# no need to transpose here!
+      if (region != "si2") {
+        population_v2 <- pop %>% filter(lon > xstart & lon < (xstart+0.005*xdef), lat < ystart & lat > (ystart-0.005*ydef))      
       } else {
-        population_v2 <- pop %>% filter(lon > xstart & lon < 180, lat < ystart & lat > (ystart-0.005*ydef))
+        if (i == 1) {
+          population_v2 <- pop %>% filter(lon > -180 & lon < -165, lat < ystart & lat > (ystart-0.005*ydef))        
+        } else {
+          population_v2 <- pop %>% filter(lon > xstart & lon < 180, lat < ystart & lat > (ystart-0.005*ydef))
+        }
+      }
+
+      if (initial_reg[initial_reg$inc == 1,]$reg[1] == i) {
+        population_v3 <- population_v2
+        rm(population) ; rm(pop)
+      } else {
+        population_v3 <- rbind(population_v3, population_v2)
+        rm(population) ; rm(pop)
       }
     }
-
-
-    if (initial_reg[initial_reg$inc == 1,]$reg[1] == i) {
-      population_v3 <- population_v2
-      rm(population) ; rm(pop)
-    } else {
-      population_v3 <- rbind(population_v3, population_v2)
-      rm(population) ; rm(pop)
-    }
+    rm(population_v2)
+    population_v3 <- population_v3 %>% arrange(desc(lat), lon)
+    
+  } else {
+    nc_data <- nc_open(paste0("/data01/julien/projects/camaflood/DAT/population/isimip2b/", SSP, "soc/population_", region, ".nc4"))
   }
-  rm(population_v2)
-  #  if (region != "si2") {
-  #print(dim(population_v3)) ; print(xdef*ydef) ## DEBUG
-  population_v3 <- population_v3 %>% arrange(desc(lat), lon)
 
-  
   # Open the position file
   pos_file <- file(paste0("/data01/julien/projects/camaflood/OUT/", region, "_position_0.005deg.bin"), open = "rb")
   pos <- readBin(pos_file, what = "integer", n = xdef*ydef, endian = "little")
   close(pos_file) ; rm(pos_file)
 
+  counter <- 0
   for (years in seq(YEAR_STA, YEAR_END)) {
-    
-    # Downscaled water file
+    counter <- counter + 1
+    # read the correct year population for SSp
+    if (SSP != "no") {
+      population <- ncvar_get(nc_data, "number_of_people", start = c(1, 1, counter), count = c(-1, -1, 1))
+      population_v3 <- 
+      tibble(lon = rep(seq(xstart, by = step, length.out = xdef), times = ydef),
+             lat = rep(seq(ystart, by = -step, length.out = ydef), each = xdef),
+             dat = as.vector(population)) # no transpose is required
+      rm(population)
+    }
+
+    # Downscaled flooded depth
     file_water <- file(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/fld_", region, "_", years, ".flood"), open = "rb")
     temp <- readBin(con = file_water, what = "numeric", n = xdef*ydef, size = 4, endian = "little")
     close(file_water) ; rm(file_water)
@@ -161,17 +192,18 @@ for (region in regions) {
     # inference
     ex <- ifelse(temp > 0, population_v3$dat, 0)
 
+    # to aggregate by countries or relative to dam position
     sum <- tibble(position = pos,
                   country = country,
                   exposure = ex)
     
-    # Summarise > country
+    # 1. Country
     country_ex        <- sum %>% filter(exposure != -9999) %>%
       group_by(country) %>% summarise(tot_ex = sum(exposure, na.rm = TRUE))
     country_ex$year   <- years
     country_ex$region <- region
-    #print(region)
-    # Summarise > position 
+
+    # 2. Position relative to dams
     position_ex        <- sum %>% filter(exposure != -9999) %>%
       group_by(position) %>% summarise(tot_ex = sum(exposure, na.rm = TRUE))
     position_ex$year   <- years
@@ -187,12 +219,23 @@ for (region in regions) {
     }
 
   } # year loop
-  if (file.exists(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/country_exposure.csv"))) {
-    write_csv(exposure_country,  paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/country_exposure.csv"), append = TRUE)
-    write_csv(exposure_position, paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/position_exposure.csv"), append = TRUE)      
+
+  if (SSP == "no") {
+    if (file.exists(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/country_exposure.csv"))) {
+      write_csv(exposure_country,  paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/country_exposure.csv"), append = TRUE)
+      write_csv(exposure_position, paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/position_exposure.csv"), append = TRUE)      
+    } else {
+      write_csv(exposure_country,  paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/country_exposure.csv"))
+      write_csv(exposure_position, paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/position_exposure.csv"))  
+    }    
   } else {
-    write_csv(exposure_country,  paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/country_exposure.csv"))
-    write_csv(exposure_position, paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/position_exposure.csv"))  
+    nc_close(nc_data) # the netcdf file is for an entire region
+    if (file.exists(paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/", SSP, "_country_exposure.csv"))) {
+      write_csv(exposure_country,  paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/", SSP, "_country_exposure.csv"), append = TRUE)
+      write_csv(exposure_position, paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/", SSP, "_position_exposure.csv"), append = TRUE)      
+    } else {
+      write_csv(exposure_country,  paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/", SSP, "_country_exposure.csv"))
+      write_csv(exposure_position, paste0("/data01/julien/projects/camaflood/OUT/global_", GCMS, EXP, "/", SSP, "_position_exposure.csv"))  
+    }
   }
-}
-# Write the final file
+} # all regions done
